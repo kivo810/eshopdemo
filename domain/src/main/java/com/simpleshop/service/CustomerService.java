7ccf9245112100957479a1ca8c4efa878b8098f9
kivo810 <kivo810@hotmail.com>
2020-04-17 18:13:07 +0200

    added init customers

--- domain/src/main/java/com/simpleshop/service/CustomerService.java
+++ domain/src/main/java/com/simpleshop/service/CustomerService.java
@@ -5,13 +5,24 @@ import com.simpleshop.model.customer.Customer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.PostConstruct;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @Service
 public class CustomerService {
 
     @Autowired
     CustomerRep customerRepository;
+
+    @PostConstruct
+    public void initCustomers(){
+        customerRepository.saveAll(Stream.of
+                (new Customer(1,"John","Cardiac", "new","xxx"),
+                        new Customer(2,"peter","eye","old","newone"))
+                .collect(Collectors.toList()));
+    }
 
     public List<Customer> getAllCustomers() {
         return customerRepository.findAll();
--- web/src/main/java/EShopApp.java
+++ web/src/main/java/EShopApp.java
@@ -1,7 +1,13 @@
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"cz.ucl.*"})
+@EntityScan(basePackages = {"cz.ucl.*"})
+@EnableJpaRepositories(basePackages = {"cz.ucl.*"})
 public class EShopApp {
 
     public static void main(String[] args) {
