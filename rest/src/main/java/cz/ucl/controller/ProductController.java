52873ce88db21ca7da947fc93eb14344421775e6
kivo810 <kivo810@hotmail.com>
2020-04-20 00:56:52 +0200

    Half of ProductController done, not tested

--- rest/src/main/java/cz/ucl/controller/ProductController.java
+++ rest/src/main/java/cz/ucl/controller/ProductController.java
@@ -3,6 +3,7 @@ package cz.ucl.controller;
 import cz.ucl.model.product.Product;
 import cz.ucl.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -20,19 +21,28 @@ public class ProductController {
 
     @GetMapping(value = "/products/{id}")
     public String getProduct(@PathVariable Integer id) {
-
         return productService.getProductFromId(id).toString();
     }
 
     @PostMapping("/products")
-    public void addProduct(@RequestBody Product product){
-//        product.setName("newname");
-//        product.setAvailable(100);
-//        product.setDescription("popoospis");
-//        product.setPrice(18.98);
-//        productService.addProduct(product);
+    public ResponseEntity<Object> addProduct(@RequestBody Product product){
+        if(product.getName() == null || product.getName().equals("")){
+            return ResponseEntity.status(400).body("Name is empty or null!");
+        }
+
+        if (product.getPrice() > 100000.00 || product.getPrice() < 0) {
+            return ResponseEntity.status(400).body("Price is below 0 or higher than 100000");
+        }
+
+        if (product.getAvailable() < 0){
+            return ResponseEntity.status(400).body("There must be at least one product");
+        }
+
         productService.addProduct(product);
+        return ResponseEntity.status(200).body(product);
     }
+
+
 
     @DeleteMapping(value = "/products/{id}")
     public void deleteProduct(@PathVariable Integer id){
