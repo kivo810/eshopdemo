702e58e7265c61d8b50912031a0e3631e12d1e42
kivo810 <kivo810@hotmail.com>
2020-04-19 22:28:10 +0200

    Completed OrderController, not tested

--- rest/src/main/java/cz/ucl/controller/OrderController.java
+++ rest/src/main/java/cz/ucl/controller/OrderController.java
@@ -1,7 +1,33 @@
 package cz.ucl.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import cz.ucl.model.shopOrder.ShopOrder;
+import cz.ucl.service.OrderService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class OrderController {
+
+    @Autowired
+    private OrderService orderService;
+
+    @GetMapping(value = "/order/{id}")
+    public ResponseEntity<Object> getOrder(HttpEntity<String> httpEntity, @PathVariable Integer id){
+        Optional<ShopOrder> order = orderService.getOrderById(id);
+        if (order.isEmpty()) {
+            return ResponseEntity.status(400).body("Order does not exist. Wrong ID.");
+        }
+        return ResponseEntity.status(200).body(order.get());
+    }
+
+    @GetMapping(value = "/order/orderByDate")
+    public List<ShopOrder> ordersByDate() {
+        return orderService.orderByCompletedAt();
+    }
+
 }
