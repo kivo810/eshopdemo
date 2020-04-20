00066c0b6e9334bb3b3bb84fbb43f7445f02dfd4
kivo810 <kivo810@hotmail.com>
2020-04-20 11:32:15 +0200

    Refactored mainApp class

--- web/src/main/java/cz/ucl/EShopApp.java
+++ web/src/main/java/cz/ucl/EShopApp.java
@@ -1,3 +1,5 @@
+package cz.ucl;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
