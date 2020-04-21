b559681dbcd0d8e31324015de6f4f1ba96a7fa1d
kivo810 <kivo810@hotmail.com>
2020-04-21 17:29:53 +0200

    TODO cart in WebController

--- web/src/main/java/cz/ucl/controller/WebController.java
+++ web/src/main/java/cz/ucl/controller/WebController.java
@@ -5,12 +5,15 @@ import cz.ucl.service.CartService;
 import cz.ucl.service.OrderService;
 import cz.ucl.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 
 @Controller
 public class WebController {
@@ -24,8 +27,7 @@ public class WebController {
     @Autowired
     private CartService cartService;
 
-//    @GetMapping(value = "/")
-    @RequestMapping("/")
+    @GetMapping(value = "/")
     public String index(Model model){
         List<Product> products = productService.getAllProducts();
         model.addAttribute("products", products);
@@ -44,8 +46,23 @@ public class WebController {
     }
 
     //TODO -- add product to cart
+    @PostMapping(value = "/add")
+    public ResponseEntity<Object> addProductToCart(@RequestParam Map<String, String> input){
+        if (productService.getProductFromId(Integer.valueOf(input.get("productId"))).isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Product does not exist. Wrong ID.");
+        }
+        Optional<Product> product = productService.getProductFromId(Integer.valueOf(input.get("productId")));
+        cartService.addProductToCart(product.get());
+        return ResponseEntity.status(HttpStatus.OK).body(product.get());
+    }
 
     //TODO -- remove product from cart
+    @DeleteMapping(value = "/remove/{id}")
+    public ResponseEntity<Object> removeProductFromCart(@PathVariable int id) {
+        cartService.deleteProductFromCartViaId(id);
+
+        return ResponseEntity.status(HttpStatus.OK).body(orderService.getOrders());
+    }
 
     //TODO -- complete order, write to DB
 
